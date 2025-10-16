d600813d783665ed8e37f830c1586f0448d0325c
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-10-16 19:14:04 +0900

    Feat: 로그인버튼 클릭시 로그인 페이지 이동기능 구현

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,26 +1,29 @@
 import { NavigationMenuDemo } from "../common/NavigationMenu";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
+
   return (
     <div>
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-1">
             <NavigationMenuDemo />
         </div>
         <div className="w-full flex justify-between items-center mt-2 px-4">
-            <div>
+            <Link to="/">
                 <Button>
                     로고
                 </Button>
-            </div>
-            <div>
+            </Link>
+            <Link to="/login">
                 <Button>
                     Login
                 </Button>
-            </div>
+            </Link>
         </div>
-        <h1 className="flex justify-center text-center text-6xl font-bold leading-relaxed
- mt-40">협업과 AI의 모든 것<br />DevChat에서 쉽고 간편하게</h1>
+        <h1 className="flex justify-center text-center text-6xl font-bold leading-relaxed mt-40">
+            협업과 AI의 모든 것<br />DevChat에서 쉽고 간편하게
+        </h1>
     </div>
   );
 }
