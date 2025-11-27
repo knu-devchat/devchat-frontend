365716d18b8e2997298d10ad2d1cb0a3b65cb0da
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-11-27 18:21:07 +0900

    프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+    },
+    '/accounts': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
