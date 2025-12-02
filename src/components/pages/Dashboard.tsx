3cdf9bc09424836eda1ae441a09a01346705e6ce
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-12-02 17:08:58 +0900

    Feat: 사이드바 토글 삭제

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -157,11 +157,6 @@ export default function Dashboard() {
       {selectedRoom ? (
         <SidebarInset>
           <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem>
